3228ec0360ba612851fb7ecac7c4713bdd32af5a
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-07-22 23:11:59 +0900

    WifiServiceクラスの接続処理を改善し、接続状態を管理するフィールドを追加

--- app/quadruped_controller/lib/services/wifi_service.dart
+++ app/quadruped_controller/lib/services/wifi_service.dart
@@ -1,13 +1,33 @@
+import 'dart:io';
+import '../constants/network.dart';
 class WifiService {
+  Socket? _socket;
+  bool _connected = false;
+
+  bool get connected => _connected;
+
   Future<void> connect() async {
-    print("Connection...");
+    try{
+      _socket = await Socket.connect(
+        NetworkConfig.host,
+        NetworkConfig.port
+      );
+      _connected = true;
+      print("Connected");
+    }catch(e){
+      print("Connection Failed: $e");
+    }
   }
 
   Future<void> disconnect() async {
+    _socket?.close();
+    _socket = null;
+    _connected = false;
     print("Disconnected");
   }
 
   Future<void> send(String command) async{
+    _socket?.write(command);
     print("send: $command");
   }
 }
